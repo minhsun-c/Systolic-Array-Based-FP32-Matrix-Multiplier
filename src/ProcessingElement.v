b7dbe74b94e537b310bb0e42d2b137482383dc4c
chmh0624 <harrychang2003@gmail.com>
2024-04-17 18:33:17 +0800

    Add forwarding path, but not working

--- src/ProcessingElement.v
+++ src/ProcessingElement.v
@@ -3,6 +3,8 @@
 
 module P_Element(
     output reg  [31:0]  OUT,
+    output      [31:0]  OUT_BOTTOM,
+    output      [31:0]  OUT_RIGHT,
     input       [31:0]  IN_TOP,
     input       [31:0]  IN_LEFT,
     input               CLK,            // Positive Edge Triggered
@@ -12,10 +14,13 @@ module P_Element(
 // ================================================================================
 // Declaration : registers, wires
 // ================================================================================
+    reg     [6:0]   CUR_STATE;
+    reg     [2:0]   REG_STATE;
     reg     [31:0]  REG_TOP;
     reg     [31:0]  REG_LEFT;
     reg     [31:0]  REG_MUL;
     reg     [31:0]  REG_ADD;
+    reg     [31:0]  FW_ADD;
     wire    [31:0]  RESULT_MUL;
     wire    [31:0]  RESULT_ADD;
     wire            RESULT_isZero;
@@ -24,12 +29,20 @@ module P_Element(
 // RESET = 0 : reset all the register to 0
 // ================================================================================
     always @(negedge RST_N) begin
+        REG_STATE   <=  3'b0;
         REG_TOP     <= 32'b0;
         REG_LEFT    <= 32'b0;
         REG_MUL     <= 32'b0;
         REG_ADD     <= 32'b0;
         OUT         <= 32'b0;
+        CUR_STATE   <= 7'b0;
+        FW_ADD      <= 32'b0;
     end
+
+    always @(posedge CLK) CUR_STATE <= CUR_STATE + 1;
+    always @(posedge CLK) 
+        if (REG_STATE < 3'd2)
+            REG_STATE <= REG_STATE + 1;
 
 // ================================================================================
 // STAGE 1: Store input to REG_TOP, REG_LEFT
@@ -38,6 +51,8 @@ module P_Element(
         REG_TOP     <= IN_TOP;
         REG_LEFT    <= IN_LEFT;
     end
+    assign OUT_BOTTOM   = IN_TOP;
+    assign OUT_RIGHT    = IN_LEFT;
 
 // ================================================================================
 // STAGE 2: REG_TOP * REG_LEFT, result stores in pipeline register (REG_MUL)
@@ -45,7 +60,7 @@ module P_Element(
     FP_Multiplier MUL(
         REG_TOP, REG_LEFT, RESULT_MUL
     );
-    always @(posedge CLK) begin
+    always @(posedge CLK) begin 
         if (~(|REG_TOP) | ~(|REG_LEFT))
             REG_MUL     <= `FPZero;
         else
@@ -57,9 +72,14 @@ module P_Element(
 // ================================================================================
     FP_Adder_Subtractor32 ADD(
         .Out(RESULT_ADD),       .isZero(RESULT_isZero),
-        .A(OUT),                .B(REG_MUL )
+        .A(FW_ADD),                .B(REG_MUL)
     );
     always @(posedge CLK) begin
+        if (REG_STATE == 3'd3)
+            FW_ADD      <= REG_ADD;
+        else
+            FW_ADD      <= `FPZero;
+
         if ((~(|OUT) & ~(|REG_MUL )) | RESULT_isZero)
             REG_ADD     <= `FPZero;
         else if (~|OUT) 
@@ -74,7 +94,8 @@ module P_Element(
 // STAGE 4: REG_ADD  -> OUT
 // ================================================================================
     always @(posedge CLK) begin
-        OUT         <= REG_ADD ;
+        OUT             <= REG_ADD ;
+        
     end
 
 endmodule

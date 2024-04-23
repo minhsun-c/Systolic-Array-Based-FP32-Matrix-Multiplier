479816c6c3ebdc46d7be53032a9eccfcf2437263
chmh0624 <harrychang2003@gmail.com>
2024-04-23 20:01:33 +0800

    Remove forwarding path, but add a stall

--- src/ProcessingElement.v
+++ src/ProcessingElement.v
@@ -11,42 +11,46 @@ module P_Element(
     input               RST_N           // Negative Edge Triggered
 );
 
-// ================================================================================
+// ==============================================================================================
 // Declaration : registers, wires
-// ================================================================================
-    reg     [6:0]   CUR_STATE;
-    reg     [2:0]   REG_STATE;
-    reg     [31:0]  REG_TOP;
-    reg     [31:0]  REG_LEFT;
-    reg     [31:0]  REG_MUL;
-    reg     [31:0]  REG_ADD;
-    reg     [31:0]  FW_ADD;
-    wire    [31:0]  RESULT_MUL;
+// ==============================================================================================
+    reg             OUT_CHECK;      // Decide `OUT` should be replaced or not
+    reg     [2:0]   CUR_STATE;      // Counter will decide whether we should add stall or not
+    reg     [31:0]  REG_TOP;        // To store input from top of PE
+    reg     [31:0]  REG_LEFT;       // To store input from left of PE
+    reg     [31:0]  REG_MUL;        // To store the product of REG_TOP, REG_LEFT
+    reg     [31:0]  REG_ADD;        // To store the sum of product, OUT
+    reg     [31:0]  REG_STALL;      // To store REG_MUL for one more cycle
+    wire    [31:0]  PRE_ADD;        // Give the correct operand for Adder, depend by CUR_STATE
+    wire    [31:0]  RESULT_MUL;     
     wire    [31:0]  RESULT_ADD;
     wire            RESULT_isZero;
 
-// ================================================================================
+// ==============================================================================================
 // RESET = 0 : reset all the register to 0
-// ================================================================================
+// ==============================================================================================
     always @(negedge RST_N) begin
-        REG_STATE   <=  3'b0;
         REG_TOP     <= 32'b0;
         REG_LEFT    <= 32'b0;
         REG_MUL     <= 32'b0;
         REG_ADD     <= 32'b0;
+        REG_STALL   <= 32'b0;
         OUT         <= 32'b0;
-        CUR_STATE   <= 7'b0;
-        FW_ADD      <= 32'b0;
+        CUR_STATE   <= 3'b0;
+        OUT_CHECK   <= 1'b0;
     end
 
-    always @(posedge CLK) CUR_STATE <= CUR_STATE + 1;
-    always @(posedge CLK) 
-        if (REG_STATE < 3'd2)
-            REG_STATE <= REG_STATE + 1;
+    always @(posedge CLK) begin
+        if (CUR_STATE <= 3'd3)
+            CUR_STATE <= CUR_STATE + 1; 
+        else
+            CUR_STATE <= CUR_STATE;
+    end
 
-// ================================================================================
+
+// ==============================================================================================
 // STAGE 1: Store input to REG_TOP, REG_LEFT
-// ================================================================================
+// ==============================================================================================
     always @(posedge CLK) begin
         REG_TOP     <= IN_TOP;
         REG_LEFT    <= IN_LEFT;
@@ -54,9 +58,9 @@ module P_Element(
     assign OUT_BOTTOM   = IN_TOP;
     assign OUT_RIGHT    = IN_LEFT;
 
-// ================================================================================
+// ==============================================================================================
 // STAGE 2: REG_TOP * REG_LEFT, result stores in pipeline register (REG_MUL)
-// ================================================================================
+// ==============================================================================================
     FP_Multiplier MUL(
         REG_TOP, REG_LEFT, RESULT_MUL
     );
@@ -67,35 +71,44 @@ module P_Element(
             REG_MUL     <= RESULT_MUL;
     end
 
-// ================================================================================
-// STAGE 3: OUT + REG_MUL , result stores in pipeline register (REG_ADD)
-// ================================================================================
+// ==============================================================================================
+// STAGE 3: STALL
+// ==============================================================================================
+    always @(posedge CLK) begin
+        REG_STALL       <= REG_MUL; 
+    end
+// ==============================================================================================
+// STAGE 4: OUT + REG_MUL , result stores in pipeline register (REG_ADD)
+// ==============================================================================================
+    assign PRE_ADD = (CUR_STATE > 3'd3) ? REG_STALL : REG_MUL;
+    
     FP_Adder_Subtractor32 ADD(
         .Out(RESULT_ADD),       .isZero(RESULT_isZero),
-        .A(FW_ADD),                .B(REG_MUL)
+        .A(OUT),                .B(PRE_ADD)
     );
     always @(posedge CLK) begin
-        if (REG_STATE == 3'd3)
-            FW_ADD      <= REG_ADD;
-        else
-            FW_ADD      <= `FPZero;
-
+        // (OUT == 0 && REG_MUL == 0) or (A + B == 0)
         if ((~(|OUT) & ~(|REG_MUL )) | RESULT_isZero)
             REG_ADD     <= `FPZero;
+        // OUT == 0
         else if (~|OUT) 
             REG_ADD     <= REG_MUL ;
+        // REG_MUL == 0
         else if (~|REG_MUL ) 
             REG_ADD     <= OUT;
         else 
             REG_ADD     <= RESULT_ADD;
     end
 
-// ================================================================================
-// STAGE 4: REG_ADD  -> OUT
-// ================================================================================
+// ==============================================================================================
+// STAGE 5: REG_ADD  -> OUT
+// ==============================================================================================
     always @(posedge CLK) begin
-        OUT             <= REG_ADD ;
-        
+        if (OUT_CHECK)
+            OUT         <= REG_ADD ;
+        else
+            OUT         <= OUT;
+        OUT_CHECK = ~OUT_CHECK;
     end
 
 endmodule
